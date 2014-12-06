8ec00d9d16e403492160f93fc31a173a4a2a9425
dimaMaltsev <maltsev.dima7@gmail.com>
2014-12-06 19:38:05 +0100

    double ground check

--- Assets/Resources/code/interfaces/BigGuy/Collider_BigGuy.cs
+++ Assets/Resources/code/interfaces/BigGuy/Collider_BigGuy.cs
@@ -7,8 +7,8 @@ public class Collider_BigGuy : MonoBehaviour {
 	void Start () {
 		PolygonCollider2D p = GetComponent<PolygonCollider2D> ();
 		p.points = new Vector2[]{ 
-			Vector2.up , Vector2.right/1.5f + Vector2.up/2, Vector2.right/1.5f, Vector2.right/1.7f - Vector2.up/1.2f, Vector2.right/2 - Vector2.up/1.1f,
-			-Vector2.right/2 - Vector2.up/1.1f, -Vector2.right/1.7f - Vector2.up/1.2f, -Vector2.right/1.5f, -Vector2.right/1.5f + Vector2.up/2
+			Vector2.right/1.7f + Vector2.up/1.11f, Vector2.right/1.5f + Vector2.up/2, Vector2.right/1.5f, Vector2.right/1.7f - Vector2.up/1.2f, Vector2.right/2 - Vector2.up/1.1f,
+			-Vector2.right/2 - Vector2.up/1.1f, -Vector2.right/1.7f - Vector2.up/1.2f, -Vector2.right/1.5f, -Vector2.right/1.5f + Vector2.up/2, -Vector2.right/1.7f + Vector2.up/1.11f
 
 		};
 
