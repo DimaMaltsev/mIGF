2d7c1dad1d6d38cfd5e3c19cc01813e5c0862fcc
dimaMaltsev <maltsev.dima7@gmail.com>
2015-01-07 23:36:04 +0100

    block corners init

--- Assets/Resources/code/specialized/blocks/Block_TypeDetection.cs
+++ Assets/Resources/code/specialized/blocks/Block_TypeDetection.cs
@@ -5,24 +5,89 @@ using System.Collections.Generic;
 public class Block_TypeDetection : MonoBehaviour {
 	private SpriteLibrary spriteLibrary;
 	private SpriteRenderer spriteRenderer;
+	public string tmp;
 
 	private Dictionary<string,int> blockTypeMap = new Dictionary<string, int>{
-		{ "0,0,0,0" , 10 },
-		{ "1,0,0,0" , 9 },
-		{ "0,1,0,0" , 6 },
-		{ "0,0,1,0" , 8 },
-		{ "0,0,0,1" , 7 },
-		{ "1,1,0,0" , 5 },
-		{ "1,0,1,0" , 12 },
-		{ "1,0,0,1" , 4 },
-		{ "0,1,1,0" , 2 },
-		{ "0,1,0,1" , 11 },
-		{ "0,0,1,1" , 3 },
-		{ "1,1,1,0" , 13 },
-		{ "1,1,0,1" , 1 },
-		{ "1,0,1,1" , 14 },
 		{ "0,1,1,1" , 0 },
+		{ "1,1,0,1" , 1 },
+		{ "0,1,1,0" , 2 },
+		{ "0,0,1,1" , 3 },
+		{ "1,0,0,1" , 4 },
+		{ "1,1,0,0" , 5 },
+		{ "0,1,0,0" , 6 },
+		{ "0,0,0,1" , 7 },
+		{ "0,0,1,0" , 8 },
+		{ "1,0,0,0" , 9 },
+		{ "0,0,0,0" , 10 },
+		{ "0,1,0,1" , 11 },
+		{ "1,0,1,0" , 12 },
+		{ "1,1,1,0" , 13 },
+		{ "1,0,1,1" , 14 },
 		{ "1,1,1,1" , 15 }
+	};
+
+	private Dictionary<string,Dictionary<string,int>> blockCornersTypeMap = new Dictionary<string, Dictionary<string,int>>{
+		// _|
+		{ 
+			"1,1,0,0", new Dictionary<string, int >{
+				{"0,0,0,0", 36}
+			}
+		},
+		{ 
+			"0,1,1,0", new Dictionary<string, int >{
+				{"0,0,0,0", 33}
+			}
+		},
+		{ 
+			"0,0,1,1", new Dictionary<string, int >{
+				{"0,0,0,0", 34}
+			}
+		},
+		{
+			"1,0,0,1", new Dictionary<string, int >{
+				{"0,0,0,0", 35}
+			}
+		},
+		// _|_
+		{
+			"1,1,0,1", new Dictionary<string, int >{
+				{"0,0,0,0", 27}
+			}
+		},
+		{
+			"0,1,1,1", new Dictionary<string, int >{
+				{"0,0,0,0", 25}
+			}
+		},
+		{
+			"1,0,1,1", new Dictionary<string, int >{
+				{"0,0,0,0", 26}
+			}
+		},
+		{
+			"1,1,1,0", new Dictionary<string, int >{
+				{"0,0,0,0", 24}
+			}
+		},
+		// .
+		{
+			"1,1,1,1", new Dictionary<string, int >{
+				{"0,0,0,0", 32},
+				{"0,0,1,0", 31},
+				{"1,0,0,0", 28},
+				{"0,1,0,0", 29},
+				{"0,0,0,1", 30},
+				{"1,1,0,0", 21},
+				{"0,1,0,1", 22},
+				{"0,0,1,1", 23},
+				{"1,0,1,0", 20},
+				{"0,1,1,1", 16},
+				{"1,1,0,1", 17},
+				{"1,1,1,0", 18},
+				{"1,0,1,1", 19}
+			}
+		}
+
 	};
 
 	void Awake(){
@@ -42,18 +107,40 @@ public class Block_TypeDetection : MonoBehaviour {
 		Collider2D[] down 	= Physics2D.OverlapPointAll( p + Vector3.down 	);
 		Collider2D[] left 	= Physics2D.OverlapPointAll( p + Vector3.left 	);
 
+		Collider2D[] upLeft 	= Physics2D.OverlapPointAll( p + Vector3.up 	+ Vector3.left	);
+		Collider2D[] upRight 	= Physics2D.OverlapPointAll( p + Vector3.up 	+ Vector3.right );
+		Collider2D[] downLeft 	= Physics2D.OverlapPointAll( p + Vector3.down 	+ Vector3.left 	);
+		Collider2D[] downRight 	= Physics2D.OverlapPointAll( p + Vector3.down	+ Vector3.right );
+
 		bool b_up 		= ThereIsABlock( up 	);
 		bool b_right 	= ThereIsABlock( right 	);
 		bool b_left 	= ThereIsABlock( left 	);
 		bool b_down 	= ThereIsABlock( down 	);
 
-		string type = 
-			( b_up ? 	"1" : "0" ) + "," + 
-			( b_right ? "1" : "0" ) + "," + 
-			( b_down ? 	"1" : "0" ) + "," + 
-			( b_left ? 	"1" : "0" );
+		bool b_up_left 		= ThereIsABlock( upLeft 	);
+		bool b_up_right 	= ThereIsABlock( upRight 	);
+		bool b_down_left 	= ThereIsABlock( downLeft 	);
+		bool b_down_right 	= ThereIsABlock( downRight 	);
 
-		spriteRenderer.sprite = spriteLibrary.blocks[ blockTypeMap[ type ] ];
+		string type = 
+			( b_up 		? "1" : "0" ) + "," + 
+			( b_right 	? "1" : "0" ) + "," + 
+			( b_down 	? "1" : "0" ) + "," + 
+			( b_left 	? "1" : "0" );
+		string cornerType =
+			( b_up_left 	? "1" : "0" ) + "," + 
+			( b_up_right 	? "1" : "0" ) + "," + 
+			( b_down_left 	? "1" : "0" ) + "," + 
+			( b_down_right 	? "1" : "0" );
+		
+		tmp = type + "   " + cornerType;
+		int outputType = blockTypeMap[ type ];
+		if(blockCornersTypeMap.ContainsKey(type)){
+			Dictionary<string,int> ct = blockCornersTypeMap[type];
+			if(ct.ContainsKey(cornerType))
+				outputType = ct[cornerType];
+		}
+		spriteRenderer.sprite = spriteLibrary.blocks[ outputType ];
 	}
 
 	private bool ThereIsABlock( Collider2D[] others ){
