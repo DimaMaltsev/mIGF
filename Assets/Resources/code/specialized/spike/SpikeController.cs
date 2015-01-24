f6281b31dd89c305ff2690ac52ed059ac31ef867
dimaMaltsev <maltsev.dima7@gmail.com>
2015-01-24 18:06:53 +0100

    spikes sounds

--- Assets/Resources/code/specialized/spike/SpikeController.cs
+++ Assets/Resources/code/specialized/spike/SpikeController.cs
@@ -11,10 +11,19 @@ public class SpikeController : MonoBehaviour {
 	private BoxCollider2D cldr;
 	private Animator a;
 
+	private AudioSource audioSource;
+	private SoundLibrary soundLibrary;
+
 	void Awake(){
 		sr = GetComponent<SpriteRenderer>();
 		cldr = GetComponent<BoxCollider2D>();
 		a = GetComponent<Animator> ();
+
+		audioSource = GetComponent<AudioSource> ();
+		GameObject soundLibGameObj = GameObject.FindGameObjectWithTag( "SoundLibrary" );
+		if( soundLibGameObj != null ){
+			soundLibrary = soundLibGameObj.GetComponent<SoundLibrary>();
+		}
 	}
 
 	void Start(){
@@ -33,14 +42,22 @@ public class SpikeController : MonoBehaviour {
 	private void Open(){
 		cldr.enabled = true;
 		a.SetBool ("opened", true);
+		PlaySound ("spikes_out");
 	}
 
 	private void Close(){
 		cldr.enabled = false;
 		a.SetBool ("opened", false);
+		PlaySound ("spikes_in");
 	}
 
 	private void ActivateTrigger(){
 		SwitchActive();
+	}
+
+	private void PlaySound(string soundName){
+		AudioClip sound = soundLibrary.GetSound( soundName , audioSource , "sfx" );
+		audioSource.clip = sound;
+		audioSource.Play ();
 	}
 }
