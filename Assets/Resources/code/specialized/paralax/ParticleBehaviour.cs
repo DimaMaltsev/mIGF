8590188ea77423d451227ffda88cd173d11278ef
dimaMaltsev <maltsev.dima7@gmail.com>
2015-02-05 21:42:16 +0100

    particles

---
+++ Assets/Resources/code/specialized/paralax/ParticleBehaviour.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ParticleBehaviour : MonoBehaviour {
+	public float lifeTime;
+	public float lifeSpeed;
+	public float xSpread;
+	public float ySpread;
+	public float xSpeed;
+	public float ySpeed;
+	public float xRand;
+	public float yRand;
+	public float alphaSpeed;
+
+	private bool living = true;
+	private float targetAlpha = 1;
+	private SpriteRenderer spriteRender;
+	private float sign = 1;
+
+	private float upperAlpha = 1;
+	private float lowerAlpha = 0.5f;
+	private bool grownParticle = false;
+
+	private float xDiff;
+	private float yDiff;
+
+	void Awake(){
+		spriteRender = GetComponent<SpriteRenderer> ();
+	}
+
+	void Start(){
+		xDiff = xSpeed / 2 - Random.Range (0.0f, xSpeed) + xSpread;
+		yDiff = ySpeed / 2 - Random.Range (0.0f, ySpeed) + ySpread;
+	}
+
+	void Update () {
+		if(lifeTime >= 0 && living){
+			lifeTime -= lifeSpeed;
+		}else{			
+			living = false;
+			sign =-1;
+		}
+
+		float alpha = spriteRender.color.a;
+		alpha += alphaSpeed * sign;
+
+		if( living && alpha > upperAlpha && grownParticle ){
+			sign = -1;
+		}
+
+		if( living && alpha < lowerAlpha && grownParticle ){
+			sign = 1;
+		}
+
+		if( !grownParticle && alpha > lowerAlpha ){
+			grownParticle = true;
+		}
+
+		if (!living && alpha < 0) {
+			Destroy(gameObject);
+		}
+
+		spriteRender.color = Color.Lerp (spriteRender.color, new Color (1, 1, 1, alpha), Time.deltaTime * 10);
+		
+		float xrnd = Random.Range (-xRand, xRand);
+		float yrnd = Random.Range (-yRand, yRand);
+
+		Vector3 position = transform.position;
+		Vector3 newPosition = new Vector3(position.x + xDiff + xrnd , position.y + yDiff + yrnd, 0);
+
+		transform.position = Vector3.Lerp(transform.position, newPosition, Time.deltaTime * 10);
+	}
+}
---
+++ Assets/Resources/code/specialized/paralax/ParticleController.cs
@@ -0,0 +1,83 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ParticleController : MonoBehaviour {
+	public List<Sprite> particleSprites = new List<Sprite> ();
+	public float widthRange = 10;
+	public float heightRange = 10;
+	public float spawnTime = 0.1f;
+	public float maxParticleCount = 100;
+
+	public float lifeTime;
+	public float lifeSpeed;
+	public float xSpeed;
+	public float ySpeed;
+	public float xSpread;
+	public float ySpread;
+
+	public float xRand;
+	public float yRand;
+
+	public float alphaSpeed;
+
+	private List<Transform> particles = new List<Transform>();
+
+	void Start(){
+		SpawnParticle ();
+	}
+
+	void Update(){
+		bool removedParticle = false;
+
+		for(int i = 0; i < particles.Count; i++ ){
+			if(particles[i] == null){
+				particles.RemoveAt(i);
+				i--;
+				removedParticle = true;
+			}
+		}
+
+		if( removedParticle ){
+			SpawnParticle();
+		}
+	}
+
+	private void SpawnParticle(){
+		if( particles.Count >= maxParticleCount ){
+			return;
+		}
+
+		GameObject particle = new GameObject ("Particle");
+		SpriteRenderer sr = particle.AddComponent<SpriteRenderer> ();
+		sr.sprite = GetSprite ();
+		sr.color = new Color (1, 1, 1, 0);
+
+		particle.transform.position = Vector3.zero;
+		particle.transform.parent = transform;
+		particle.transform.localPosition = GetPosition();
+
+		ParticleBehaviour pb = particle.AddComponent<ParticleBehaviour> ();
+		pb.lifeTime = lifeTime;
+		pb.xSpread = xSpread;
+		pb.ySpread = ySpread;
+		pb.xSpeed = xSpeed;
+		pb.ySpeed = ySpeed;
+		pb.xRand = xRand;
+		pb.yRand = yRand;
+		pb.alphaSpeed = alphaSpeed;
+		pb.lifeSpeed = lifeSpeed;
+
+		particles.Add (particle.transform);
+
+		Invoke ("SpawnParticle", spawnTime);
+	}
+
+	private Vector3 GetPosition(){
+		return new Vector3 (widthRange/2 - Random.Range(0, widthRange), heightRange/2 - Random.Range(0, heightRange), 0);
+	}
+
+	private Sprite GetSprite(){
+		return particleSprites[Random.Range(0,particleSprites.Count)];
+	}
+}
