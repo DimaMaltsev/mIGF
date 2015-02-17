4171b94a48b7ce71e0464a6dc0ae093b99666445
dimaMaltsev <maltsev.dima7@gmail.com>
2015-02-17 21:41:35 +0100

    tutTi 5 and 6 dialogs go

---
+++ Assets/Resources/code/specialized/dialogs/Tut5Ti.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Tut5Ti : CutSceneTrigger {
+	public Vector2 startCameraShift;
+	void Awake(){
+		dialog = new List<string[]>{ // possible emotions {"sad", "happy", "angry", "neutral"}
+			new string[]{	"Ti"	, "I might be wrong, but it seems to me that...", "normal"},
+			new string[]{	"Ti"	, "these tunnels have been intentionally placed in places like this!", "neutral"},
+			new string[]{	"Ti"	, "Can't the exits NOT lead to precipices!?", "neutral"}
+		};
+		base.cameraShift = startCameraShift;
+	}
+}
---
+++ Assets/Resources/code/specialized/dialogs/Tut6Ti.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Tut6Ti : CutSceneTrigger {
+	public Vector2 startCameraShift;
+	void Awake(){
+		dialog = new List<string[]>{ // possible emotions {"sad", "happy", "angry", "neutral"}
+			new string[]{	"Ti"	, "Oh, I smell Gwo!", "normal"},
+			new string[]{	"Ti"	, "Lord, a bath would do him good, it's horrible!", "sad"},
+			new string[]{	"Ti"	, "At this rate, we will never reach the humans...", "sad"},
+			new string[]{	"Ti"	, "...they will plainly quicken themselves...", "neutral"},
+			new string[]{	"Ti"	, "...twenty times as soon as they smell Gwo's odor!", "angry"},
+			new string[]{	"Ti"	, "I'd better hurry up!", "normal"},
+		};
+		base.cameraShift = startCameraShift;
+	}
+}
